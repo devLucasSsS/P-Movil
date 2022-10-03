2518a3beca20212258228a3ca7ce590214519020
devLucasSsS <107324428+devLucasSsS@users.noreply.github.com>
2022-10-03 20:05:24 -0300

    BottonNav

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'HOLA',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -52,11 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+
       _counter++;
     });
   }
---
+++ lib/screen/BottomNav.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottonNav extends StatefulWidget {
+  const BottonNav({super.key});
+
+  @override
+  State<BottonNav> createState() => _BottonNavState();
+}
+
+class _BottonNavState extends State<BottonNav> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
