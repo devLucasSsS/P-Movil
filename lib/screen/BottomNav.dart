aef355ddfd57e5f5e6680ceb7435752c9a63ca9b
devLucasSsS <107324428+devLucasSsS@users.noreply.github.com>
2022-10-03 21:30:02 -0300

    create two screens

--- lib/screen/BottomNav.dart
+++ lib/screen/BottomNav.dart
@@ -1,17 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screen/ListProducts.dart';
+import 'package:flutter_application_1/screen/CreateProduct.dart';
+import 'ListProducts.dart';
+import 'CreateProduct.dart';
 
-//dsadasdasdad
-//hola
-class BottonNav extends StatefulWidget {
-  const BottonNav({super.key});
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
 
   @override
-  State<BottonNav> createState() => _BottonNavState();
+  State<BottomNav> createState() => _BottomNavState();
 }
 
-class _BottonNavState extends State<BottonNav> {
+class _BottomNavState extends State<BottomNav> {
+  int _currentIndex = 0;
+  List<Widget> _pages = [
+    CreateProduct(),
+    ListProducts(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.construction),
+            label: 'Crear',
+            backgroundColor: Colors.red,
+            
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt_outlined),
+            label: 'Listar',
+            backgroundColor: Colors.red
+          ),
+        ],
+        currentIndex: _currentIndex,
+        onTap: (index){
+          print(index);
+          setState(() {
+            _currentIndex = index;
+          });
+        }
+      ),
+    );
   }
 }
